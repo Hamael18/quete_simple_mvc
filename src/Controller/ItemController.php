6062beb7d2f86ac1662985ef926435082e3d45b6
Julie Trannois <trannois.julie@gmail.com>
2018-10-01 16:15:33 +0200

    Quête MVC

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,8 +1,25 @@
 <?php
 // src/Controller/ItemController.php
-require __DIR__ . '/../Model/ItemManager.php';
+namespace Controller;
 
-$items = selectAllItems();
+use Model;
+
+$itemManager = new \Model\ItemManager();
+
+$items = $itemManager->selectAllItems();
+
 
 require __DIR__ . '/../View/item.php';
+
+
+
+    class ItemController
+    {
+        public function index() {
+
+        }
+    }
+
+
+
 ?>
