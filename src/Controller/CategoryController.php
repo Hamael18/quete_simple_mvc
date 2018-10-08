46aed62c4690cc24dbcf12489e0d6b22daa27edd
Julie Trannois <trannois.julie@gmail.com>
2018-10-08 16:10:26 +0200

    First commit MVC 4

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,29 +1,31 @@
 <?php
 // src/Controller/CategoryController.php
 namespace Controller;
-
 use Model;
-
-
-
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 
 class CategoryController
 {
-    public function index() {
-        $categoryManager = new Model\CategoryManager();
-        $categories = $categoryManager->selectAllCategories();
-        require __DIR__ . '/../View/category.php';
-    }
-    public function show(int $id)
+    private $twig;
+
+    public function __construct()
     {
-        $categoryManager = new Model\CategoryManager();
-        $category = $categoryManager->selectOneCategory($id);
-
-        require __DIR__ . '/../View/showCategory.php';
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../View');
+        $this->twig = new Twig_Environment($loader);
     }
+        public function index()
+        {
+            $categoryManager = new Model\CategoryManager();
+            $categories = $categoryManager->selectAllCategories();
+            return $this->twig->render('category.html.twig', ['categories' => $categories]);
+        }
+
+        public function show($id)
+        {
+            $categoryManager = new Model\CategoryManager();
+            $category = $categoryManager->selectOneCategory($id);
+            return $this->twig->render('showCategory.html.twig', ['category' => $category]);
+        }
 }
-
-
-
-?>
